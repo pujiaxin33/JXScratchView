e556a7f356860de6eee15082f11d9b2496fc807c
pjx <pujx@jingbo360.com>
2018-06-25 11:01:46 +0800

    Initial Commit

---
+++ JXScratchTicketView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JXScratchTicketView
+//
+//  Created by jiaxin on 2018/6/25.
+//  Copyright © 2018年 jiaxin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
